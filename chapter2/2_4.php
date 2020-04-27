3759f1e24c43b48b29a17140518ba59493abcd1c
eeuoo <lee512joo@gmail.com>
2020-04-28 00:00:20 +0900

    2020-04-28

---
+++ chapter2/2_4.php
@@ -0,0 +1,5 @@
+<?php
+if (strlen(trim($_POST['zipcode'])) != 5) {
+    print "우편번호를 5자리로 입력해주세요";
+}
+?>
---
+++ chapter2/2_5.php
@@ -0,0 +1,5 @@
+<?php
+if ($_POST['email'] == 'Cloudshadow@gmail.com') {
+    print "환영합니다. 000님 !";
+}
+?>
