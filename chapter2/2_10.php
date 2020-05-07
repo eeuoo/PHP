10289e87f458fffd18ff1958791981da134a83dd
eeuoo <lee512joo@gmail.com>
2020-05-07 22:56:17 +0900

    2020-05-07

---
+++ chapter2/2_10.php
@@ -0,0 +1,5 @@
+<?php
+print strtolower('Beef, CHICKEN, Pork, duCK');
+print strtoupper('Beef, CHICKEN, Pork, duCK');
+print ucwords(strtolower('JOHN FRANKEBHIDF'));
+?>
--- chapter2/2_8.php
+++ chapter2/2_8.php
@@ -5,4 +5,10 @@ $day = 6;
 $year = 2007;
 
 printf('우편번호는 %05d이고 날짜는 %02d/%02d/%d', $zip, $month, $day, $year);
+
+$min = - 40;
+$max = 40;
+
+printf('컴퓨터는 섭씨 %+d도와 %+d도 사이에서 작동할 수 있습니다.', $min, $max);
+
 ?>
